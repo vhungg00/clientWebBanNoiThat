fa6da891df79bc17d3dd88af7fbb09de75a87786
vhungg00 <81247920+vhungg00@users.noreply.github.com>
2022-06-23 11:44:03 +0700

    hung

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,115 +1,37 @@
-import React from "react";
-import { useState } from "react";
-import styled from "styled-components";
-import PageHero from "../components/PageHero";
-import queryString from "query-string";
+import { useForm } from "react-hook-form";
+
+import styles from './Auth.module.scss';
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles)
+
 function Auth() {
- 
-  const [isLogin, setIsLogin] = useState(true);
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = (data) => console.log(data);
+  const name = watch('name');
+  const email = watch('email');
   return (
-    <Wrapper>
-      <div className="auth">
-        <PageHero tytle={"Auth"} />
-        <div className="container-auth">
-          <div className="grid wide">
-            <form action="#" className="auth__form">
-              <div className="auth__form-wrapper">
-                <div className="auth__form-switch">
-                  <h3
-                  className= {isLogin ? "active": ""}
-                   onClick={() => setIsLogin(true)}>Login</h3>
-                  <span></span>
-                  <h3
-                  className= {!isLogin ? " active": ""}
-                   onClick={() => setIsLogin(false)}>Register</h3>
-                </div>
-                <div className="form__group">
-                  {!isLogin && 
-                  <input
-                    type="text"
-                    className="form__group-inputElement"
-                    placeholder="Name"
-                  />}
-                  <input
-                    type="text"
-                    className="form__group-inputElement"
-                    placeholder="Email"
-                  />
-                </div>
-                <div className="form__group">
-                  <input
-                    type="text"
-                    className="form__group-inputElement"
-                    placeholder="Password"
-                  />
-                </div>
-                <button
-                 className="btn" type="submit">
-                  {isLogin ? "Login" : "Register"}
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </Wrapper>
+    <div className={cx('wrapper')}>
+      <h3 style={{ marginTop: "100px" }}>xin chao</h3>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input defaultValue='name' {...register("name")} />
+        <p>{name}</p>
+        {/* include validation with required or other standard HTML validation rules */}
+        <input {...register("email", {
+          required: true, pattern: {
+            value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            message: 'Please enter a valid email',
+          },
+        })} />
+        {/* errors will return when field validation fails  */}
+        {errors.password && <span></span>}
+
+        <input type="submit" />
+      </form>
+
+    </div>
   );
 }
-
-const Wrapper = styled.section`
-  .auth__form {
-    .auth__form-wrapper {
-      width: 80%;
-    }
-    padding: 2rem;
-    border: 1px solid #ccc;
-    min-width: 300px;
-    width: 60%;
-    margin: 62px auto;
-    display: flex;
-    justify-content: center;
-    .auth__form-switch {
-      display: flex;
-      justify-content: center;
-      position: relative;
-      h3 {
-        border: none;
-        background: none;
-        font-size: 2rem;
-        font-weight: 700;
-        cursor: pointer;
-        margin: 0;
-      }
-        .active {
-        color: var(--primary-color);
-      }
-      span {
-        display: block;
-        width: 2px;
-        height: 20px;
-        margin: 0 20px;
-        background-color: var(--primary-color);
-      }
-    }
-    .form__group {
-      .form__group-inputElement {
-        width: 100%;
-        outline: none;
-        margin-top: 20px;
-        padding: 12px 10px;
-        border: 1px solid #ccc;
-      }
-    }
-  }
-  @media (min-width: 776px) {
-    justify-content: flex-end;
-  }
-  .btn {
-    width: 100%;
-    margin-top: 1rem;
-    text-align: center;
-    font-weight: 700;
-  }
-`;
 
 export default Auth;
